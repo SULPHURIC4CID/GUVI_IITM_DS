9d2ea397025588d6427abfae35ae7d0455f3e505
SULPHURIC4CID <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-04-10 23:57:27 +0530

    Add files via upload

---
+++ Codekata/Strings/sttrings87.py
@@ -0,0 +1,15 @@
+s = input();
+try:
+    if int(s[0]+s[1]) <= 31:
+        if int((s[3]+s[4]) <= 12:
+            if int(s[6]+s[7]+s[8]+s[9]) <= 9999:
+               print('yes');
+            else:
+                print('no');
+        else:
+            print('no');
+    else:
+        print('no');
+except:
+    print('no');
+               
