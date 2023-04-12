72ee79c801116d162d0103c98df3eefb555d41c1
SULPHURIC4CID <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-04-12 09:40:15 +0530

    Add files via upload

---
+++ Codekata/Strings/strings95.py
@@ -0,0 +1,5 @@
+s = input();
+month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+print(month[int(s[3]+s[4])-1]);
+         
+        
